1399582670e5ce31468e885d011c86c8ad827527
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-11-01 19:33:07 +0600

    daisy ui install and router setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-
-import './App.css';
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/router";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
