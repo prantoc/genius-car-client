13ce87df7ba18fefa781511ab2f3fc81dfde0222
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-11-03 15:26:57 +0600

    get orders based on the user email

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,11 +1,20 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+import { errorToast } from '../../toast/Toaster';
 
 const Orders = () => {
     const { user } = useContext(AuthContext);
+    const [orders, setOrders] = useState({});
+    useEffect(() => {
+        fetch(`http://localhost:5000/orders?email=${user.email}`)
+            .then(res => res.json())
+            .then(data => setOrders(data))
+            .catch(err => errorToast(err))
+    }, [user?.email])
+
     return (
         <div>
-            orders
+            {orders.length}
         </div>
     );
 };
