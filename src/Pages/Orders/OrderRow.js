ed5992c7dabec52124b786ab0ac5752abf4ff0f5
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-11-03 16:03:27 +0600

    show service img

--- src/Pages/Orders/OrderRow.js
+++ src/Pages/Orders/OrderRow.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { errorToast } from '../../toast/Toaster';
 
 const OrderRow = ({ order }) => {
     console.log(order)
     const { service, serviceName, price, customer, email, phone } = order
+    const [serviceDetails, setServiceDetails] = useState([])
+    useEffect(() => {
+        fetch(`http://localhost:5000/service/${service}`)
+            .then(res => res.json())
+            .then(data => setServiceDetails(data))
+            .catch(err => errorToast(err))
+    }, [service])
     return (
         <tr>
             <th>
@@ -12,11 +20,11 @@ const OrderRow = ({ order }) => {
             </th>
             <td>
                 <div className="flex items-center space-x-3">
-                    {/* <div className="avatar">
-                        <div className="mask mask-squircle w-12 h-12">
-                            <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                    <div className="avatar">
+                        <div className="rounded w-48 h-36">
+                            <img src={serviceDetails.img} alt="Avatar Tailwind CSS Component" />
                         </div>
-                    </div> */}
+                    </div>
                     <div>
                         <div className="font-bold">{serviceName}</div>
                         <div className="text-sm opacity-60">#{service}</div>
